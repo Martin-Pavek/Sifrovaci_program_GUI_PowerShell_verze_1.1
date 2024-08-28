f4b6b03802f28e6961c52b17df219c64f6f3b1ee
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-08-28 12:23:59 +0200

    Add files via upload

--- generuj_klic.bat
+++ generuj_klic.bat
@@ -3,7 +3,7 @@ title generator klice
 cls
 dir keys
 set /p nazev_klice="nazev klice ? "
-powershell -file ./generuj_klic.ps1 %nazev_klice%
-REM generuj_klic.com %nazev_klice%
+REM powershell -file ./generuj_klic.ps1 %nazev_klice%
+generuj_klic.com %nazev_klice%
 sleep 1
 @echo on
