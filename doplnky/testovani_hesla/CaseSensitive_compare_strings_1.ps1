f65dd4394bf768fb4fa2079d6d2805c8c7a35b3d
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-10-14 04:22:56 +0200

    Add files via upload

---
+++ doplnky/testovani_hesla/CaseSensitive_compare_strings_1.ps1
@@ -0,0 +1,23 @@
+﻿cls
+# dalsi vec, porovani stringu bez a z CaseSensitive -ceq operatorem ( default je off )
+"a" -like "a"
+"a" -like "A"
+#
+"aa" -like "aa"
+"aa" -like "AA"
+#
+"a" -clike "a"
+"a" -clike "A" # False
+"a" -eq "a"
+"a" -eq "A"
+#
+echo 'CaseSensitive compare strings "-ceq" oparator'
+
+"a" -ceq "a" # stejne 
+"a" -ceq "A" # tady uz dobre, vylozene se mu musi rict aby rozlisoval velky a maly znaky !
+"aA" -like "Aa"
+"aA" -clike "Aa"
+"aA" -like "aA"
+
+sleep 10
+
