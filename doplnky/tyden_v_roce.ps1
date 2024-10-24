0649640ff616fe1313baea248a0d41d94406bd0c
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-10-24 06:11:18 +0200

    Add files via upload

---
+++ doplnky/tyden_v_roce.ps1
@@ -0,0 +1,11 @@
+﻿cls
+$ted = (get-date)
+echo $ted
+
+$den_v_roce = $ted.DayOfYear
+echo "je $den_v_roce den v roce"
+
+[int32] $tyden_v_roce = (( $den_v_roce / 7 ))
+echo "cislo tydne je $tyden_v_roce"
+
+sleep 10
